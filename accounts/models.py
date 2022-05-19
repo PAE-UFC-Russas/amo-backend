11226166ee2ab58a57a771c2cec448ba42fb6d6d
Juan Monteiro <juan.monteiro@jmonteiro.net>
2022-05-19 20:21:07 -0300

    corrige criação do usuário

--- accounts/models.py
+++ accounts/models.py
@@ -4,12 +4,15 @@ from django.utils.translation import gettext_lazy as _
 
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, date_of_birth, password=None, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
-        return self._create_user(email, email, password, **extra_fields)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
 
-    def create_superuser(self, email, date_of_birth, password=None, **extra_fields):
+    def create_superuser(self, email, password=None, **extra_fields):
         # create superuser here
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
@@ -19,7 +22,7 @@ class CustomUserManager(BaseUserManager):
         if extra_fields.get("is_superuser") is not True:
             raise ValueError("Superuser must have is_superuser=True.")
 
-        return self._create_user(email, email, password, **extra_fields)
+        return self.create_user(email, password, **extra_fields)
 
 
 class CustomUser(AbstractUser):
