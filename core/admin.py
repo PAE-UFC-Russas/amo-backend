0cf0559dfe5bebde7f3c2bf88531a29870580e08
Juan Monteiro <juan.monteiro@jmonteiro.net>
2022-05-19 21:18:29 -0300

    adiciona modelo 'Curso'

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from core.models import Curso
 
-# Register your models here.
+admin.register(Curso)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Curso(models.Model):
+    nome = models.TextField()
+    descricao = models.TextField()
+
+    def __str__(self):
+        return f"Curso: {self.nome}"
