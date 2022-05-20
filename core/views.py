28636c0e6aa5d9c039e7b9139d4e526b91478ead
Juan Monteiro <juan.monteiro@jmonteiro.net>
2022-05-19 21:19:25 -0300

    adiciona view com lista de cursos

--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from core.models import Curso
+from core.serializer import CursoSerializer
 
-# Create your views here.
+
+class CursoViewSet(ViewSet):
+    def list(self, request):
+        cursos = Curso.objects.all()
+        serializer = CursoSerializer(cursos, many=True)
+        return Response(serializer.data)
