9b448d3a38f77bff9bc7f3d7fd5b097a31587ad5
Juan Monteiro <juan.monteiro@jmonteiro.net>
2022-05-24 20:37:46 -0300

    exige autenticação para acessar Cursos

--- core/views.py
+++ core/views.py
@@ -1,11 +1,15 @@
+from ast import Is
 from django.shortcuts import render
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from core.models import Curso
 from core.serializer import CursoSerializer
 
 
 class CursoViewSet(ViewSet):
+    permission_classes = [IsAuthenticated]
+
     def list(self, request):
         cursos = Curso.objects.all()
         serializer = CursoSerializer(cursos, many=True)
