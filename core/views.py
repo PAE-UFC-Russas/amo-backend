3ee016b9b0d06be344ce8f0d219fcaee066939d5
Pedro Henrique de Sousa Alcantara <50456948+alcantar0@users.noreply.github.com>
2022-05-25 08:47:31 -0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ViewSet
-
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 from rest_framework.decorators import action
 
