e2982dce107283af349618794a1fc0c9004ea715
Juan Monteiro <juan.monteiro@jmonteiro.net>
2022-05-26 19:59:30 -0300

    desfaz mudança no serializer do curso

--- core/serializer.py
+++ core/serializer.py
@@ -2,13 +2,14 @@ from rest_framework import serializers
 
 from core.models import Curso
 
-class CursoSerializer(serializers.Serializer):
+
+class CursoSerializer(serializers.HyperlinkedModelSerializer):
     nome = serializers.CharField()
     descricao = serializers.CharField()
+
     def create(self, validated_data):
         return Curso.objects.create(**validated_data)
+
     class Meta:
         model = Curso
-        fields = ['__all__']
-
-
+        fields = ["id", "nome", "descricao"]
