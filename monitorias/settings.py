20e00db521039194d9edd2a33fdeb3093080df97
Juan Monteiro <13399354+monteirojuan@users.noreply.github.com>
2022-05-26 22:43:13 -0300

    Documentação online (#10)
    * adiciona drf_spectacular como dependencia
    
    * ativa documentação online
    
    * corrige erros de conflito no merge anterior
    
    * Ajusta CursoViewSet para exibir na documentação
    
    * Adiciona instruções para acessar a documentação

--- monitorias/settings.py
+++ monitorias/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "rest_framework.authtoken",
     "accounts",
     "core",
+    "drf_spectacular",
 ]
 
 MIDDLEWARE = [
@@ -132,5 +133,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
-    ]
+    ],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
--- monitorias/urls.py
+++ monitorias/urls.py
@@ -13,24 +13,25 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from core.views import CursoViewSet
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerSplitView
 from rest_framework import routers
 from rest_framework.authtoken import views as auth_views
-import core
-from core.views import CursoViewSet
-
-from core.views import CursoViewSet
 
 router = routers.DefaultRouter()
-router.register(r"cursos", CursoViewSet, basename="cursos")
-
-
 router.register(r"cursos", CursoViewSet, basename="cursos")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/auth/", auth_views.obtain_auth_token, name="obtain-api-token"),
-    path("", include(router.urls )),
-    path("cursos/add/", include('core.urls'))
+    path("", include(router.urls)),
+    # documentação/drf_spectacular
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/schema/openapi/",
+        SpectacularSwaggerSplitView.as_view(url_name="schema"),
+        name="openapi",
+    ),
 ]
