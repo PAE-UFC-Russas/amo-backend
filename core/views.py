785d187a9e9765749c0b99b9f6f74982ab91a495
Juan Monteiro <juan.monteiro@jmonteiro.net>
2022-05-26 21:11:34 -0300

    Ajusta CursoViewSet para exibir na documentação

--- core/views.py
+++ core/views.py
@@ -1,4 +1,3 @@
-
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
@@ -8,14 +7,17 @@ from core.serializer import CursoSerializer
 
 class CursoViewSet(ViewSet):
     permission_classes = [IsAuthenticated]
+    serializer_class = CursoSerializer
 
     def list(self, request):
+        """Retorna uma lista de Cursos"""
         cursos = Curso.objects.all()
         serializer = CursoSerializer(cursos, many=True)
         return Response(serializer.data)
 
     def create(self, request):
-        serializer=CursoSerializer(data=request.data)
+        """Adiciona um Curso"""
+        serializer = CursoSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
         return Response(serializer.data)
