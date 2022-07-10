ea80feafba6b625c99c1b66a9b3867e9c0e38adf
Juan Monteiro <juan.monteiro@jmonteiro.net>
2022-07-10 11:30:21 -0300

    Cria token de autenticação ao criar usuário

---
+++ accounts/signals.py
@@ -0,0 +1,13 @@
+"""Define signal handlers para o módulo 'accounts'"""
+
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+
+
+@receiver(signal=post_save, sender=settings.AUTH_USER_MODEL)
+def create_user_token(created=False, instance=None, **kwargs):
+    """Cria um token de autenticação após o registro do usuário."""
+    if created:
+        Token.objects.create(user=instance)
