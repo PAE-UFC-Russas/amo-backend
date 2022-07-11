7d28d0ad458b42e9660f27282352ba229c3626ca
pedro <alcantarapedro69@yahoo.com>
2022-07-11 13:04:12 -0300

    Resolvido length

--- forum_amo/models.py
+++ forum_amo/models.py
@@ -7,12 +7,12 @@ class Duvida(models.Model):
 
     """Modelo para dúvidas"""
 
-    titulo = models.CharField(max_lenght=None)
-    descricao = models.TextField(max_lenght=550)
+    titulo = models.CharField(max_length=None)
+    descricao = models.TextField(max_length=550)
     ESCOLHAS_PRIVACIDADE = ((0, 'Ocultar privacidade'),
                             (1, 'Não ocultar privacidade'),)
 
-    privacidade_autor = PositiveSmallIntegerField(max_lenght=1, choices=ESCOLHAS_PRIVACIDADE)
+    privacidade_autor = PositiveSmallIntegerField(max_length=1, choices=ESCOLHAS_PRIVACIDADE)
 
     
 # Create your models here.
