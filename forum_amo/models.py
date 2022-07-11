fff6a085097b2876a3f0314be1bf2cbd65476154
pedro <alcantarapedro69@yahoo.com>
2022-07-11 17:19:47 -0300

    Privacidade do autor como BooleanField

--- forum_amo/models.py
+++ forum_amo/models.py
@@ -4,17 +4,14 @@ Models para o app forum_app
 
 from django.db import models
 
-ESCOLHAS_PRIVACIDADE = ((0, 'Ocultar privacidade'),
-                           (1, 'Não ocultar privacidade'),)
-
-
 class Duvida(models.Model):
 
     """Modelo para dúvidas"""
 
     titulo = models.CharField(max_length=200)
     descricao = models.TextField(max_length=550)
-    privacidade_autor = models.PositiveSmallIntegerField(choices=ESCOLHAS_PRIVACIDADE)
+    privacidade_autor = models.BooleanField(default=True)
+
 
     
 # Create your models here.
