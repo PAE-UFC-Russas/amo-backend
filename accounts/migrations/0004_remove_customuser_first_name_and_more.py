c53451dc321628cd2a484231ea223f989a8a063e
Juan Monteiro <juan.monteiro@jmonteiro.net>
2022-07-13 23:31:38 -0300

    Formata migração 004

--- accounts/migrations/0004_remove_customuser_first_name_and_more.py
+++ accounts/migrations/0004_remove_customuser_first_name_and_more.py
@@ -8,29 +8,52 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0002_disciplinas'),
-        ('accounts', '0003_customuser_is_email_active'),
+        ("core", "0002_disciplinas"),
+        ("accounts", "0003_customuser_is_email_active"),
     ]
 
     operations = [
         migrations.RemoveField(
-            model_name='customuser',
-            name='first_name',
+            model_name="customuser",
+            name="first_name",
         ),
         migrations.RemoveField(
-            model_name='customuser',
-            name='last_name',
+            model_name="customuser",
+            name="last_name",
         ),
         migrations.CreateModel(
-            name='Perfil',
+            name="Perfil",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('nome_completo', models.CharField(max_length=255)),
-                ('nome_exibicao', models.CharField(max_length=32)),
-                ('data_nascimento', models.DateField(null=True)),
-                ('matricula', models.CharField(max_length=6, null=True)),
-                ('curso', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to='core.curso')),
-                ('usuario', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, related_name='perfil', to=settings.AUTH_USER_MODEL)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("nome_completo", models.CharField(max_length=255)),
+                ("nome_exibicao", models.CharField(max_length=32)),
+                ("data_nascimento", models.DateField(null=True)),
+                ("matricula", models.CharField(max_length=6, null=True)),
+                (
+                    "curso",
+                    models.ForeignKey(
+                        blank=True,
+                        null=True,
+                        on_delete=django.db.models.deletion.SET_NULL,
+                        to="core.curso",
+                    ),
+                ),
+                (
+                    "usuario",
+                    models.OneToOneField(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="perfil",
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
             ],
         ),
     ]
