ee8e56319831a3101558cb41cac7f647854411cf
pedro <alcantarapedro69@yahoo.com>
2022-07-27 08:39:49 -0300

    Erro testes resolvido

--- forum_amo/tests.py
+++ forum_amo/tests.py
@@ -2,22 +2,28 @@
 TESTS forum_app
 """
 from django.urls import reverse
-from rest_framework import status
 from rest_framework.test import APITestCase
 from forum_amo.models import Duvida
-from django.contrib.auth.models import AnonymousUser, User
-from accounts.models import CustomUser
 from forum_amo.serializers import DuvidaSerializer
+from accounts.models import CustomUser
 
 class DuvidaTestes(APITestCase):
-    def set_up(self):
-        self.user = CustomUser.objects.create(email='johndoe@localhostt.com', password='password123')
-        
-        self.admin = CustomUser.objects.create_superuser(email='superjohndoe@localhost.com', password='superpassword123')
-            
-        self.duvida = Duvida.objects.create(titulo='Distribuição Exponencial', descricao='Probabilidade e Estatística')
+    def setUp(self) -> None:
+        self.user = CustomUser.objects.create(
+            email="johndoe@localhost.com", password="password123"
+        )
 
-    def listar_duvidas(self):
-        response = self.client.get(reverse('duvida-list'),
-                                   HTTP_AUTHORIZATION = f"Token {self.user.auth_token.key}",) 
+        self.admin = CustomUser.objects.create_superuser(
+            email="superjohndoe@localhost.com", password="superpassword123"
+        )
+
+        self.duvida = Duvida.objects.create(
+            titulo="Distribuição Exponencial", descricao="Probabilidade e Estatística"
+        )
+
+    def test_listar_duvidas(self):
+        response = self.client.get(
+            reverse("duvidas-list"),
+            HTTP_AUTHORIZATION=f"Token {self.user.auth_token.key}",
+        )
         self.assertEqual(response.data, [DuvidaSerializer(self.duvida).data])
