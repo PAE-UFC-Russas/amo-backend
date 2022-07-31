834c91a579109eb86067c79519630c9878f74452
pedro <alcantarapedro69@yahoo.com>
2022-07-31 08:37:52 -0300

    IsAuthenticatedOrReadOnly

--- forum_amo/views.py
+++ forum_amo/views.py
@@ -5,17 +5,16 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
-
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from forum_amo.models import Duvida
 from forum_amo.serializers import DuvidaSerializer
 
 
 class DuvidaViewSet(ModelViewSet):
     """ViewSet referente ao modelo de dúvidas do fórum"""
-    
+
     authentication_classes = [SessionAuthentication, BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     serializer_class = DuvidaSerializer
     queryset = Duvida.objects.all()
