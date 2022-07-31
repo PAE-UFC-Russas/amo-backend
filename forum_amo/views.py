c697cef699027478e41c6db94fbe4045b60d0d79
pedro <alcantarapedro69@yahoo.com>
2022-07-31 07:58:31 -0300

    Permissões e autenticação adicionadas. Porém erro nos testes (Token)

--- forum_amo/views.py
+++ forum_amo/views.py
@@ -4,6 +4,8 @@ View forum_app
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from forum_amo.models import Duvida
 from forum_amo.serializers import DuvidaSerializer
@@ -11,6 +13,9 @@ from forum_amo.serializers import DuvidaSerializer
 
 class DuvidaViewSet(ModelViewSet):
     """ViewSet referente ao modelo de dúvidas do fórum"""
+    
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
     serializer_class = DuvidaSerializer
     queryset = Duvida.objects.all()
