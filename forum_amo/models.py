247f50d650af303567a46dca6a10097317025c3f
Alanis Oliveira <alanisoliveira39@gmail.com>
2022-08-03 15:45:33 -0300

    Adiciona autor à dúvida

--- forum_amo/models.py
+++ forum_amo/models.py
@@ -3,7 +3,7 @@ Models para o app forum_app
 """
 
 from django.db import models
-
+from accounts.models import CustomUser
 
 class Duvida(models.Model):
 
@@ -11,6 +11,7 @@ class Duvida(models.Model):
 
     titulo = models.CharField(max_length=200)
     descricao = models.TextField(max_length=550)
+    autor = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=False)
 
     def __str__(self):
         return f"Dúvida: {self.titulo}. Descricão: {self.descricao}"
