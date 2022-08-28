0c764ec4b63d6cab8fdd43eb56df615f03c234dc
Juan Monteiro <juan.monteiro@jmonteiro.net>
2022-08-28 12:08:40 -0300

    Permite ordenação de dúvidas pela data de criação.

--- forum_amo/models.py
+++ forum_amo/models.py
@@ -13,6 +13,7 @@ class Duvida(models.Model):
 
     titulo = models.CharField(max_length=200)
     descricao = models.TextField(max_length=550)
+    data = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"Dúvida: {self.titulo}. Descricão: {self.descricao}"
