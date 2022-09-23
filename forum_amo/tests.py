09d8f64a4ed8545dc784c9efc071a0c35b91338b
Juan Monteiro <13399354+monteirojuan@users.noreply.github.com>
2022-09-22 22:55:35 -0300

    ativa a paginação nas listas (#72)
    * ativa a paginação nas listas
    
    * corrige testes

--- forum_amo/tests.py
+++ forum_amo/tests.py
@@ -1,6 +1,7 @@
 """
 TESTS forum_app
 """
+import json
 
 from django.urls import reverse
 from rest_framework import status
@@ -45,7 +46,10 @@ class DuvidaTestes(APITestCase):
         response = self.client.get(
             reverse("duvidas-list"), HTTP_AUTHORIZATION=f"Token {self.user_token}"
         )
-        self.assertEqual(response.data, [DuvidaSerializer(self.duvida).data])
+        self.assertEqual(
+            json.loads(response.content)["results"],
+            [DuvidaSerializer(self.duvida).data],
+        )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_criar_duvida(self):
@@ -120,7 +124,10 @@ class RespostaTestes(APITestCase):
             reverse("respostas-list"),
             HTTP_AUTHORIZATION=f"Token {self.user.auth_token}",
         )
-        self.assertEqual(response.data, [RespostaSerializer(self.resposta).data])
+        self.assertEqual(
+            json.loads(response.content)["results"],
+            [RespostaSerializer(self.resposta).data],
+        )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_criar_resposta(self):
--- monitorias/settings.py
+++ monitorias/settings.py
@@ -147,6 +147,8 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
     ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 25,
 }
 
 FIXTURE_DIRS = ["fixtures"]
