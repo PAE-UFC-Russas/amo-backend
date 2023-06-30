d999df10561b54338cd573b358a6c89c8486a2ff
Pedro Henrique de Sousa Alcântara <alcantarapedro69@yahoo.com>
2023-06-29 22:12:41 -0300

    Mudança no Fórum: resposta_correta está como blank=True (ou seja, quando a dúvida for criada, não é necessário ligar essa dúvida com uma respostas, pois ainda pode não haver nenhuma resposta pra ela.

--- forum_amo/models.py
+++ forum_amo/models.py
@@ -16,8 +16,10 @@ class Duvida(models.Model):
     descricao = models.TextField(max_length=550)
     data = models.DateTimeField(auto_now=True)
     disciplina = models.ForeignKey(Disciplinas, on_delete=models.CASCADE)
+    #Ao usuário criar a dúvida, ainda não haverá respostas, assim não é possivel adicionar uma resposta_correta.
+    #Por isso está como blank=True
     resposta_correta = models.ForeignKey(
-        "Resposta", on_delete=models.SET_NULL, null=True, related_name="+"
+        "Resposta", on_delete=models.SET_NULL, null=True, blank=True related_name="+"
     )
     autor = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=False)
     votos = models.IntegerField(default=0)
