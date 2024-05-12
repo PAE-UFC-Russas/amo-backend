18feb448972cba0951c38363221545dad5e01095
Pedro Henrique de Sousa Alcântara <alcantarapedro69@yahoo.com>
2024-05-12 16:58:40 -0300

    único arquivo de testes para registro de usuário

--- accounts/tests/test_registration_user.py
+++ accounts/tests/test_registration_user.py
@@ -1,11 +1,9 @@
-# pylint: skip-file
-# accounts/tests/test_registration_user.py
+"""Testes Registro e Ativação de Email"""
 from rest_framework.test import APITestCase
-from django.urls import reverse
 from rest_framework import status
+from django.urls import reverse
 from accounts.models import CustomUser
 from accounts.models import EmailActivationToken
-import datetime
 
 
 class UserRegistrationTest(APITestCase):
@@ -26,22 +24,36 @@ class UserRegistrationTest(APITestCase):
             "Verifique seu e-mail para ativar sua conta", response.data["message"]
         )
 
+    def test_email_activation(self):
+        """Verifica a confirmação do email"""
+        # verifica que email não foi confirmado
+        user = CustomUser.objects.first()
+        self.assertFalse(user.is_email_active)
+
+        # faz a ativação
+        activation_token = EmailActivationToken.objects.first()
+        # self.client.credentials(HTTP_AUTHORIZATION=f"Token {user.auth_token.key}")
+        response = self.client.post(
+            "/registrar/confirmar-email/",
+            {"token": f"{activation_token.token}"},
+        )
+
+        # verifica se foi ativado com sucesso
+        user.refresh_from_db()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(user.is_email_active)
+
     def test_email_confirmation(self):
         """Testa a confirmação de email do usuário."""
         # Confirmando o e-mail
-    
         confirm_data = EmailActivationToken.objects.get(email="newuser@example.com")
-        print(confirm_data.token)
-        #{"token": self.email_token.token}
-        data = {'token': confirm_data.token}
-        response = self.client.post("/registrar/confirmar-email/", data, format='json')
-        #response = self.client.post("/api/user/confirmar-email/", confirm_data.token)
+        data = {"token": confirm_data.token}
+        response = self.client.post("/registrar/confirmar-email/", data, format="json")
+        # response = self.client.post("/api/user/confirmar-email/", confirm_data.token)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIn("E-mail confirmado com sucesso", response.data["message"])
 
         self.assertIsNotNone(
             confirm_data, "Token de confirmação de e-mail não foi gerado."
         )
-
-
         self.assertIn("auth_token", str(response.content))
