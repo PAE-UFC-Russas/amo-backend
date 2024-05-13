2965c1ddc4ce70a914df642fa93e913ac2f88678
Pedro Henrique de Sousa Alcântara <alcantarapedro69@yahoo.com>
2024-05-12 21:40:49 -0300

    test_authentication

--- accounts/tests/test_authentication.py
+++ accounts/tests/test_authentication.py
@@ -2,26 +2,38 @@
 
 from django.urls import reverse
 from rest_framework.test import APITestCase
-
-from accounts import account_management_service
+from accounts.models import CustomUser, EmailActivationToken
 
 PASSWORD = "M@vr8RjZS8LqrjhV"
 
 
 class UserAuthTest(APITestCase):
-    """Verifica a autenticação do usuário."""
+    """Verifica o login e autenticação do usuário."""
 
     fixtures = ["groups.yaml"]
 
     def setUp(self):
-        _, self.token = account_management_service.create_account(
-            sanitized_email_str="test@user.com",
-            unsafe_password_str=PASSWORD,
+        self.client.post(
+            reverse("registrar-list"),
+            {"email": "test@user.com", "password": PASSWORD},
+            format="json",
         )
+
+        self.user = CustomUser.objects.first()
+
+        activation_token = EmailActivationToken.objects.first()
+        response = self.client.post(
+            "/registrar/confirmar-email/",
+            {"token": f"{activation_token.token}"},
+        )
+
+        form = {"username": "test@user.com", "password": PASSWORD}
+        response = self.client.post("/usuario/login/", data=form)
+        self.user_auth_token = response.json()["token"]
 
     def test_get_user_api_key(self):
         """Verifica que o processo de login retorna uma chave da API válida."""
 
         data = {"username": "test@user.com", "password": PASSWORD}
         response = self.client.post(reverse("obtain-api-token"), data, format="json")
-        self.assertEqual(response.data["token"], self.token)
+        self.assertEqual(response.data["token"], self.user_auth_token)
