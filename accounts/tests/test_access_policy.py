4318a0021fab9dafc34502f7747bfd537be35e4d
Pedro Henrique de Sousa Alcântara <alcantarapedro69@yahoo.com>
2024-05-12 22:10:19 -0300

    access policy

--- accounts/tests/test_access_policy.py
+++ accounts/tests/test_access_policy.py
@@ -1,12 +1,10 @@
-# pylint: skip-file
-
 """Testes de controle de acesso do aplicativo 'accounts'."""
 
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from accounts import account_management_service
+from accounts.models import CustomUser, EmailActivationToken
 
 # from accounts.models import EmailActivationToken
 from core.models import Curso
@@ -18,13 +16,27 @@ class UserAccessPolicyTestCase(APITestCase):
     """Verifica o controle de acesso para UserViewSet"""
 
     fixtures = ["groups.yaml"]
-    # def test_criar(self):
-    #     response = self.client.post(reverse("registrar-list"), {"email": "user2@pae.localhost", "password": "ajfsd9p&*aa"})
-    #     assert response.status_code == 201
+
     def setUp(self) -> None:
-        self.user_model = account_management_service.create_account(
-            sanitized_email_str="user@localhost", unsafe_password_str=PASSWORD
+        self.email = "user@localhost.com"
+        self.password = PASSWORD
+        self.client.post(
+            reverse("registrar-list"),
+            {"email": "user@localhost.com", "password": PASSWORD},
+            format="json",
         )
+
+        self.user = CustomUser.objects.first()
+
+        activation_token = EmailActivationToken.objects.first()
+        response = self.client.post(
+            "/registrar/confirmar-email/",
+            {"token": f"{activation_token.token}"},
+        )
+
+        form = {"username": "user@localhost.com", "password": PASSWORD}
+        response = self.client.post("/usuario/login/", data=form)
+        self.user_auth_token = response.json()["token"]
 
         Curso.objects.create(nome="Computação", descricao="Computação")
 
@@ -36,15 +48,15 @@ class UserAccessPolicyTestCase(APITestCase):
                 {"email": "user2@pae.localhost", "password": "ajfsd9p&*aa"},
             )
             self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        # with self.subTest("Ativar email"):
-        #     response = self.client.post(reverse("registrar-confirmar-email"))
-        #     self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        with self.subTest("Ativar email"):
+            response = self.client.post("/registrar/confirmar-email/")
+            self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         with self.subTest("Login"):
             # A view de login é fornecida pelo Django Rest Framework, por isso
             # não é controlada por UserViewAccessPolicy
             response = self.client.post(
-                reverse("obtain-api-token"),
-                {"username": "user@localhost", "password": PASSWORD},
+                "/usuario/login/",
+                {"username": "user@localhost.com", "password": PASSWORD},
             )
             self.assertEqual(response.status_code, status.HTTP_200_OK)
 
@@ -70,31 +82,30 @@ class UserAccessPolicyTestCase(APITestCase):
             response = self.client.post(
                 reverse("registrar-list"),
                 {"email": "user1@localhost", "password": "f7aw87ho2q!"},
-                HTTP_AUTHORIZATION=f"Token {self.token}",
+                HTTP_AUTHORIZATION=f"Token {self.user_auth_token}",
             )
             self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-        # with self.subTest("Ativar email"):
-        #     email_token = EmailActivationToken.objects.create(
-        #         user=self.user, email=self.user.email, token="000000"
-        #     )
-        #     response = self.client.post(
-        #         reverse("registrar-confirmar-email"),
-        #         {"token": email_token.token},
-        #         HTTP_AUTHORIZATION=f"Token {self.token}",
-        #     )
-        #     self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        with self.subTest("Ativar email"):
+            email_token = EmailActivationToken.objects.create(
+                user=self.user, email=self.user.email, token="000000"
+            )
+            response = self.client.post(
+                "/registrar/confirmar-email/",
+                {"token": email_token.token},
+            )
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
         with self.subTest("Login"):
             response = self.client.post(
                 reverse("obtain-api-token"),
-                {"username": "user@localhost", "password": PASSWORD},
-                HTTP_AUTHORIZATION=f"Token {self.token}",
+                {"username": "user@localhost.com", "password": PASSWORD},
+                HTTP_AUTHORIZATION=f"Token {self.user_auth_token}",
             )
             self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         with self.subTest("Listar"):
             response = self.client.get(
                 reverse("usuario-list"),
-                HTTP_AUTHORIZATION=f"Token {self.token}",
+                HTTP_AUTHORIZATION=f"Token {self.user_auth_token}",
             )
             self.assertEqual(response.status_code, status.HTTP_200_OK)
 
@@ -102,15 +113,11 @@ class UserAccessPolicyTestCase(APITestCase):
             response = self.client.get(
                 reverse("usuario-list"),
                 args=[1],
-                HTTP_AUTHORIZATION=f"Token {self.token}",
+                HTTP_AUTHORIZATION=f"Token {self.user_auth_token}",
             )
             self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         with self.subTest("Atualizar usuário"):
-            _, token = account_management_service.create_account(
-                sanitized_email_str="user_patch@localhost",
-                unsafe_password_str=PASSWORD,
-            )
             response = self.client.patch(
                 reverse("usuario-detail", args=["eu"]),
                 {
@@ -122,19 +129,15 @@ class UserAccessPolicyTestCase(APITestCase):
                     "ano_entrada": "2022.1",
                 },
                 format="json",
-                HTTP_AUTHORIZATION=f"Token {token}",
+                HTTP_AUTHORIZATION=f"Token {self.user_auth_token}",
             )
             self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         with self.subTest("Atualizar outro usuário"):
-            _, token = account_management_service.create_account(
-                sanitized_email_str="user_patch2@localhost",
-                unsafe_password_str=PASSWORD,
-            )
             response = self.client.patch(
                 reverse("usuario-detail", args=[2]),
                 {"perfil": {"matricula": "000002"}},
                 format="json",
-                HTTP_AUTHORIZATION=f"Token {token}",
+                HTTP_AUTHORIZATION=f"Token {self.user_auth_token}",
             )
             self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
