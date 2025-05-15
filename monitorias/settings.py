82850895bf4674f6f3a9d9207faa2dbee7ecf870
Pedro Henrique de Sousa Alcântara <alcantarapedro69@yahoo.com>
2025-05-15 14:18:16 -0300

    black

--- monitorias/settings.py
+++ monitorias/settings.py
@@ -66,13 +66,12 @@ else:
     # pylint: disable=C0301
 
     DATABASES = {
-       "default": {
-           "ENGINE": "django.db.backends.sqlite3",
-           "NAME": BASE_DIR + "db.sqlite3",
-      }
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR + "db.sqlite3",
+        }
     }
-    #DATABASES = {"default": dj_database_url.config(default=(os.getenv("DB_URL")))}
-
+    # DATABASES = {"default": dj_database_url.config(default=(os.getenv("DB_URL")))}
 
 
 # Application definition
